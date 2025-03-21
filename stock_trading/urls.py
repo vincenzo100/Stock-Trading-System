6296db117c03865f5292007c871a75d1a52304ac
keatienz <atienza.kacellyn@gmail.com>
2025-03-20 21:12:51 -0700

    Update urls.py
    undid my changes

--- stock_trading/urls.py
+++ stock_trading/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('', home),  # Default API Home Route
     path('admin/', admin.site.urls),  # Admin Panel
     path('api/', include('trade_app.urls')),  # Includes API routes from trade_app
-    path('', include('stock_trading.urls')), #KEA 03/20/2025 - needed to display webpages
 ]
 
 # Debugging Check: Ensure `trade_app.urls` is correctly loaded
